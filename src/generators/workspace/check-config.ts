17580310989f81eafc3fe455a9539be85178663e
jessiewebcode <jessiewebcode@gmail.com>
2022-03-31 18:02:09 +0200

    fix commands error on windows

--- src/generators/workspace/check-config.ts
+++ src/generators/workspace/check-config.ts
@@ -1,18 +1,17 @@
 import { IBitoviConfig, IBiWorkspace, log } from '../../core';
 import { existsSync, readFileSync } from 'fs';
-import { normalize } from 'path';
+import { normalize, join } from 'path';
 
 export function getExistingBiConfig(): IBiWorkspace {
   let commandPath: string = process.cwd();
-
-  if (!existsSync(`${commandPath}/bi.json`)) {
+  const biConfigPath = normalize(join(commandPath, 'bi.json'));
+  console.log(biConfigPath);
+  if (!existsSync(biConfigPath)) {
     commandPath = searchBiWorkspaceRoot();
   }
 
   try {
-    const bitoviConfig: IBitoviConfig = JSON.parse(
-      readFileSync(`${commandPath}/bi.json`).toString()
-    );
+    const bitoviConfig: IBitoviConfig = JSON.parse(readFileSync(biConfigPath).toString());
 
     const workspaceConfig: IBiWorkspace = {
       biConfig: bitoviConfig,
@@ -28,9 +27,9 @@ export function getExistingBiConfig(): IBiWorkspace {
 }
 
 function searchBiWorkspaceRoot(): string {
-  const commandPath = normalize(process.cwd()).split('/');
-
-  for (const folder of commandPath) {
+  const commandPath = normalize(process.cwd()).split(process.cwd().includes('/') ? '/' : '\\');
+  console.log(commandPath);
+  for (let index = commandPath.length - 1; index >= 0; index--) {
     commandPath.pop();
     const upperPath: string = commandPath.join('/');
 
